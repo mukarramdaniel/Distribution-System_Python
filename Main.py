9222dcb56f981dd8072b3025070d49edef231f38
Mukarram Ali <mukarramali623@gmail.com>
2022-11-27 10:47:45 -0800

    Design-UserInterfaces

--- Main.py
+++ Main.py
@@ -4,14 +4,38 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox, QGraphicsDropShadowEffect,QMessageBox
 )
+from PyQt5.QtCore import QPropertyAnimation
 
 class MainWindow(QMainWindow):
     def __init__(self,parent=None):
         QMainWindow.__init__(self)
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
-
+        self.ui.btnUpdateStock.clicked.connect(lambda: self.OpenUpdatePage())
+        self.ui.btnBuyStock.clicked.connect(lambda: self.OpenBuyStockPage())
+        self.ui.btn_Report_Cost.clicked.connect(lambda: self.OpenReportCostPage())
+        self.ui.menuBtn.clicked.connect(lambda: self.slideLeftMenu())
         self.show()
+    def OpenBuyStockPage(self):
+        self.ui.mainBody.setCurrentIndex(0)
+    def OpenUpdatePage(self):
+        self.ui.mainBody.setCurrentIndex(1)
+    def OpenReportCostPage(self):
+        self.ui.mainBody.setCurrentIndex(2)
+    def slideLeftMenu(self):
+        width=self.ui.frame_8.width()
+        if(width==0):
+            newWidth=200
+            self.ui.menuBtn.setIcon(QtGui.QIcon(u":/blackicons/Graphics/featherBlack/chevron-left.svg"))
+        else:
+            newWidth=0
+            self.ui.menuBtn.setIcon(QtGui.QIcon(u":/blackicons/Graphics/featherBlack/align-left.svg"))
+        self.animation = QPropertyAnimation(self.ui.frame_8, b"maximumWidth")
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)#Start value is the current menu width
+        self.animation.setEndValue(newWidth)#end value is the new menu width
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
