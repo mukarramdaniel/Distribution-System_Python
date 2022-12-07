91eb3a01dffe5f2ec874736e84678391b72a94d6
Mukarram Ali <mukarramali623@gmail.com>
2022-12-07 03:53:00 -0800

    Dev-U31-UI

--- Main.py
+++ Main.py
@@ -15,6 +15,61 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.Card1.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.Card2.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.Card3.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.Card4.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.widget_TopClients.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.frame_TableWhatsNew.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.frame_GoalsCompleted.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.Account_Widget.setGraphicsEffect(self.shadow)
 
         self.shadow = QGraphicsDropShadowEffect(self)
         self.shadow.setBlurRadius(30)
@@ -33,15 +88,19 @@ class MainWindow(QMainWindow):
         #self.ui.btnUpdateStock.clicked.connect(lambda: self.OpenUpdatePage())
         #self.ui.btnBuyStock.clicked.connect(lambda: self.OpenBuyStockPage())
         #self.ui.btn_Report_Cost.clicked.connect(lambda: self.OpenReportCostPage())
+        self.ui.menuBtn.clicked.connect(lambda: self.slideLeftMenu())
         self.ui.menuBtn_2.clicked.connect(lambda: self.slideLeftMenu())
+        self.ui.accountBtn.clicked.connect(lambda: self.SlideRightMenu())
         self.ui.accountBtn_3.clicked.connect(lambda: self.SlideRightMenu())
+        self.ui.btnAddEmployee.clicked.connect(lambda: self.OpenAddEmployee())
+        self.ui.btnDashboard.clicked.connect(lambda: self.OpenDashboardManager())
         self.show()
-    def OpenBuyStockPage(self):
+
+    def OpenDashboardManager(self):
         self.ui.mainBody.setCurrentIndex(0)
-    def OpenUpdatePage(self):
-        self.ui.mainBody.setCurrentIndex(1)
-    def OpenReportCostPage(self):
-        self.ui.mainBody.setCurrentIndex(2)
+    def OpenAddEmployee(self):
+        self.ui.mainBody.setCurrentIndex(3)
+    
     def slideLeftMenu(self):
         width=self.ui.LeftMenu.width()
         if(width==0):
