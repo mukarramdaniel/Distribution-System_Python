34896269d62167d1c891487c426b99a68cacab97
Ammadaslam7 <115212005+Ammadaslam7@users.noreply.github.com>
2022-12-12 23:32:00 +0500

    Dev-U02-logic

--- Login_Code.py
+++ Login_Code.py
@@ -11,32 +11,37 @@ from PyQt5.QtGui import (QColor)
 import re  #imported to validate he email
 from Manager_UI import ManaMainWindow
 #import Manager_UI
+import os
+from email.message import EmailMessage
+import smtplib
+from random import randint
 class MainWindow(QMainWindow):
+    code = 0
     def __init__(self,parent=None):
         QMainWindow.__init__(self)
         self.ui=Ui_LoginWindow()
         self.ui.setupUi(self)
-
         self.ui.forgotbtn.clicked.connect(lambda: self.OpenForgetScreen())
         self.ui.loginbtn.clicked.connect(lambda: self.Handle_Login())
         self.ui.backbtn.clicked.connect(lambda: self.BactTo_Login())
+        self.ui.sendbtn.clicked.connect(lambda: self.send_resetmail(self.ui.emailtxt.text() , 6))
+
         self.show()
     
     
         
     def Handle_Login(self):
-        #   self.ui.passwordinp.setText('sdfsdf')
         
-        # Email=self.ui.emailinp.text()
-        # Password=self.ui.passwordinp.text()
-        # flag=self.Validate_Email(Email,Password)
-        # if(flag):
-        #     QMessageBox.information(self,'Info','user and password ok')
+        Email=self.ui.emailinp.text()
+        Password=self.ui.passwordinp.text()
+        flag=self.Validate_Email(Email,Password)
+        if(flag):
+            QMessageBox.information(self,'Info','user and password ok')
             
-        # else:
-        #     QMessageBox.warning(self,'Error','Incorrect Email and Password')
-        #     self.ui.emailinp.clear()
-        #     self.ui.passwordinp.clear()
+        else:
+            QMessageBox.warning(self,'Error','Incorrect Email and Password')
+            self.ui.emailinp.clear()
+            self.ui.passwordinp.clear()
         self.Open_Manager_Window()
             
     def Validate_Email(self,Email,password):
@@ -46,18 +51,45 @@ class MainWindow(QMainWindow):
                 return True  
         else:   
             return False 
-    
+    def send_resetmail(self , email , n) :
+        start = 10**(n-1)
+        end = (10**n)-1
+        code = randint(start,end)
+        email_sender = "rasheedrayan514@gmail.com"
+        email_password = 'zpzsrqeasritevbu'
+        email_receiver = email
+
+        subject = "Forget Password"
+        body = '''
+        Dear User,
+        Your new password of the account is '''+str(code)+'''Enter this now to have access to your account.
+        Regards,
+        ARM limited
+        '''
+        em = EmailMessage()
+        em['from'] = email_sender
+        em['to'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
+        with smtplib.SMTP_SSL('smtp.gmail.com' , 465 ) as smtp:
+            smtp.login(email_sender , email_password)
+            smtp.sendmail(email_sender , email_receiver , em.as_string())
+        self.Open_newpassword_Screen()
+    #def create_newpassword(password) :
+
+
     def Open_Manager_Window(self):
         self.MainWindow = QtWidgets.QMainWindow()
         self.ui=ManaMainWindow()
         #self.ui.setupUi(self.MainWindow)
-        self.MainWindow.show()
+        #self.MainWindow.show()
         
        
 
     def OpenForgetScreen(self):
         self.ui.loginStackedWidget.setCurrentIndex(1)
-        
+    def Open_newpassword_Screen(self):
+        self.ui.loginStackedWidget.setCurrentIndex(2)
     def BactTo_Login(self):
         self.ui.loginStackedWidget.setCurrentIndex(0)
         
