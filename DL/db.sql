389cb536d97e66c10cd7ca77aefd5c8f1e6c5786
Ammadaslam7 <115212005+Ammadaslam7@users.noreply.github.com>
2022-12-14 00:16:12 +0500

    Dev-U03-logic

--- DL/db.sql
+++ DL/db.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `attendence`
+--
+
+DROP TABLE IF EXISTS `attendence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attendence` (
+  `userID` int NOT NULL,
+  `date` varchar(45) NOT NULL,
+  `time` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendence`
+--
+
+LOCK TABLES `attendence` WRITE;
+/*!40000 ALTER TABLE `attendence` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `employee`
 --
 
@@ -36,8 +59,7 @@ CREATE TABLE `employee` (
   `resetToken` varchar(45) DEFAULT NULL,
   `created_on` varchar(45) NOT NULL,
   `update_on` varchar(45) NOT NULL,
-  `date` varchar(45) NOT NULL,
-  `time` varchar(45) NOT NULL,
+  `dateTime` varchar(45) NOT NULL,
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -48,8 +70,37 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES (1,'','','','',0,'','','','',NULL,'','','','');
+INSERT INTO `employee` VALUES (2,'mukarramali623@gmail.com','employee12345','1','John',34,'03157963703','mukarramali623@gmail.com','3130258066436','435545345454',NULL,'15-01-2022 08:15:11','15-01-2022 08:15:11','15-01-2022 08:15:11');
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `prodCategory` varchar(45) NOT NULL,
+  `buyPrice` int NOT NULL,
+  `profitMargin` int DEFAULT NULL,
+  `shoeSize` int NOT NULL,
+  `selPrice` int DEFAULT NULL,
+  `color` varchar(45) NOT NULL,
+  `prodID` int NOT NULL,
+  PRIMARY KEY (`prodID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES ('Joggers',3000,35,8,3035,'black',1),('sandles',3100,40,9,NULL,'brown',2);
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -73,8 +124,6 @@ CREATE TABLE `manager` (
   `resetToken` varchar(45) DEFAULT NULL,
   `created_on` varchar(45) NOT NULL,
   `update_on` varchar(45) NOT NULL,
-  `date` varchar(45) NOT NULL,
-  `time` varchar(45) NOT NULL,
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -85,8 +134,76 @@ CREATE TABLE `manager` (
 
 LOCK TABLES `manager` WRITE;
 /*!40000 ALTER TABLE `manager` DISABLE KEYS */;
-INSERT INTO `manager` VALUES (1,'rasheedrayan514@gmail.com','admin12345',0,'Rayan Rasheed','40','03157963703','rasheedrayan514@gmail.com','3130258066437','102934848943834','12345678','10-12-2022','10-12-2022','','');
+INSERT INTO `manager` VALUES (1,'rasheedrayan514@gmail.com','admin12345',0,'Rayan Rasheed','40','03157963703','rasheedrayan514@gmail.com','3130258066437','102934848943834','12345678','10-12-2022','10-12-2022');
 /*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rider`
+--
+
+DROP TABLE IF EXISTS `rider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rider` (
+  `userID` int NOT NULL,
+  `userName` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `userRole` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `age` varchar(45) NOT NULL,
+  `contactNum` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Cnic` varchar(45) NOT NULL,
+  `bankAccount` varchar(45) NOT NULL,
+  `resetToken` varchar(45) NOT NULL,
+  `created_on` varchar(45) NOT NULL,
+  `updated_on` varchar(45) NOT NULL,
+  `date` varchar(45) DEFAULT NULL,
+  `time` varchar(45) DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `longitutde` float DEFAULT NULL,
+  `fieldArea` varchar(45) DEFAULT NULL,
+  `VehicleNumber` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rider`
+--
+
+LOCK TABLES `rider` WRITE;
+/*!40000 ALTER TABLE `rider` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `spare`
+--
+
+DROP TABLE IF EXISTS `spare`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `spare` (
+  `prodCategory` varchar(45) NOT NULL,
+  `buyPrice` int NOT NULL,
+  `profiMargin` int DEFAULT NULL,
+  `shoeSize` float NOT NULL,
+  `sellPrice` int DEFAULT NULL,
+  `color` varchar(45) NOT NULL,
+  `prodID` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `spare`
+--
+
+LOCK TABLES `spare` WRITE;
+/*!40000 ALTER TABLE `spare` DISABLE KEYS */;
+INSERT INTO `spare` VALUES ('Pumpy',3050,130,8,3180,'black',0),('Joggers',2090,120,9,2210,'brown',0);
+/*!40000 ALTER TABLE `spare` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -156,4 +273,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-13 16:07:02
+-- Dump completed on 2022-12-14  0:08:55
