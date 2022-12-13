83e3155a0dd48851d4b8eaa93687bc53b4d78f9d
Ammadaslam7 <115212005+Ammadaslam7@users.noreply.github.com>
2022-12-13 14:32:00 +0500

    Dev-U02-logic

--- Login_Code.py
+++ Login_Code.py
@@ -1,3 +1,4 @@
+
 from UI.Login import Ui_LoginWindow
 from DL.UserCRUD import *
 import sys
@@ -8,10 +9,8 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import QPropertyAnimation
 from PyQt5.QtGui import (QColor)
-#import mysql.connector()
 import re  #imported to validate he email
 from Manager_UI import ManaMainWindow
-#import Manager_UI
 import os
 from email.message import EmailMessage
 import smtplib
@@ -26,9 +25,10 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.ui.forgotbtn.clicked.connect(lambda: self.OpenForgetScreen())
         self.ui.loginbtn.clicked.connect(lambda: self.Handle_Login())
-        self.ui.backbtn.clicked.connect(lambda: self.BactTo_Login())
+        self.ui.backbtn.clicked.connect(lambda: self.BactTo_Login(0))
         self.ui.sendbtn.clicked.connect(lambda: self.send_resetmail(self.ui.emailtxt.text() , 6))
-
+        self.ui.resetbtn.clicked.connect(lambda: self.create_newpassword(self.ui.passwordtxt.text() , self.ui.emailtxt.text()))
+        self.ui.backbtn_2.clicked.connect(lambda: self.BackTo_Login(1))
         self.show()
     
     
@@ -37,7 +37,7 @@ class MainWindow(QMainWindow):
         
         Email=self.ui.emailinp.text()
         Password=self.ui.passwordinp.text()
-        flag=self.Validate_Email(Email,Password)
+        flag=self.Validate_Email_password(Email,Password)
         if(flag):
             retrive=self.user.verify(Email,Password)
             print(retrive.userRole)
@@ -50,7 +50,7 @@ class MainWindow(QMainWindow):
             self.ui.emailinp.clear()
             self.ui.passwordinp.clear()
             
-    def Validate_Email(self,Email,password):
+    def Validate_Email_password(self,Email,password):
         regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
         if(re.search(regex,Email)):
             if(len(password)>8):
@@ -63,7 +63,7 @@ class MainWindow(QMainWindow):
         code = randint(start,end)
         email_sender = "rasheedrayan514@gmail.com"
         email_password = 'zpzsrqeasritevbu'
-        email_receiver = email
+        email_receiver = self.validate_email(email) 
 
         subject = "Forget Password"
         body = '''
@@ -81,7 +81,25 @@ class MainWindow(QMainWindow):
             smtp.login(email_sender , email_password)
             smtp.sendmail(email_sender , email_receiver , em.as_string())
         self.Open_newpassword_Screen()
-    #def create_newpassword(password) :
+    def validate_email(self,email) :
+        regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+        if (re.search(regex,email)):
+            return True
+        else:
+            return False
+    def create_newpassword(self , password , email) :
+        if(str(self.code) == self.ui.codetxt.text()) :
+            if (self.ui.passwordtxt.text() != '' ):
+                if (password == self.ui.confirmtxt.text()):
+                    changed_user = self.user.getUserReturn(email)
+                    changed_user.password = password
+                    #update()
+                else :
+                    QMessageBox.warning(self,'Error','Password must be same')
+            else :
+                QMessageBox.warning(self,'Error','Password cannot be empty')
+        else:
+            QMessageBox.warning(self,'Error','Incorrect code')
 
 
     def Open_Manager_Window(self):
@@ -89,15 +107,12 @@ class MainWindow(QMainWindow):
         self.ui=ManaMainWindow()
         #self.ui.setupUi(self.MainWindow)
         #self.MainWindow.show()
-        
-       
-
     def OpenForgetScreen(self):
         self.ui.loginStackedWidget.setCurrentIndex(1)
     def Open_newpassword_Screen(self):
         self.ui.loginStackedWidget.setCurrentIndex(2)
-    def BactTo_Login(self):
-        self.ui.loginStackedWidget.setCurrentIndex(0)
+    def BactTo_Login(self , n):
+        self.ui.loginStackedWidget.setCurrentIndex(n)
         
     
 if __name__=="__main__":
