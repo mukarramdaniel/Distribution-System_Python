6319b545f307c11c2b4d7d0b6da05ffdc29a3d85
Mukarram Ali <mukarramali623@gmail.com>
2022-12-15 15:03:21 -0800

    Dev-U19-UI

--- Rider_Code.py
+++ Rider_Code.py
@@ -21,7 +21,59 @@ class RiderMainWindow(QMainWindow):
         self.shadow.setColor(QColor("black"))
         self.ui.widget_15.setGraphicsEffect(self.shadow)
 
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.widget_27.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.widget_28.setGraphicsEffect(self.shadow)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor("black"))
+        self.ui.widget_42.setGraphicsEffect(self.shadow)
+
+        self.ui.menuBtn_8.clicked.connect(lambda: self.slideLeftMenu())
+        self.ui.menuBtn_11.clicked.connect(lambda: self.slideLeftMenu())
+        self.ui.accountBtn_8.clicked.connect(lambda: self.SlideRightMenu())
+        self.ui.accountBtn_11.clicked.connect(lambda: self.SlideRightMenu())
+
         self.show()
+    def slideLeftMenu(self):
+        width=self.ui.LeftMenu.width()
+        if(width==0):
+            newWidth=220
+            #self.ui.menuBtn_2.setIcon(QtGui.QIcon(u":/blackicons/Graphics/featherBlack/chevron-left.svg"))
+        else:
+            newWidth=0
+            #self.ui.menuBtn.setIcon(QtGui.QIcon(u":/blackicons/Graphics/featherBlack/align-left.svg"))
+        self.animation = QPropertyAnimation(self.ui.LeftMenu, b"maximumWidth")
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)#Start value is the current menu width
+        self.animation.setEndValue(newWidth)#end value is the new menu width
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
+    def SlideRightMenu(self):
+        width=self.ui.profileCont.width()
+        if(width==0):
+            newWidth=105
+        else:
+            newWidth=0
+        self.animation = QPropertyAnimation(self.ui.profileCont, b"maximumWidth")
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)#Start value is the current menu width
+        self.animation.setEndValue(newWidth)#end value is the new menu width
+        self.animation.setEasingCurve(QtCore.QEasingCurve.Type.InOutQuart)
+        self.animation.start()
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
